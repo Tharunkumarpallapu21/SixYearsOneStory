d52e977ba8b6b6661e0f0e6f8e510f87eb5b0ce6
Tharunkumarpallapu21 <tharunpallapu2006@gmail.com>
2026-06-12 17:09:53 +0530

    Phase 3A birthday engine

--- app/src/main/java/com/sixyears/onestory/activities/BirthdayActivity.kt
+++ app/src/main/java/com/sixyears/onestory/activities/BirthdayActivity.kt
@@ -1,6 +1,9 @@
 package com.sixyears.onestory.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.sixyears.onestory.R
 
@@ -9,8 +12,19 @@ class BirthdayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(
-            R.layout.activity_birthday
-        )
+        setContentView(R.layout.activity_birthday)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            startActivity(
+                Intent(
+                    this,
+                    FinalSurpriseActivity::class.java
+                )
+            )
+
+            finish()
+
+        }, 5000)
     }
 }
