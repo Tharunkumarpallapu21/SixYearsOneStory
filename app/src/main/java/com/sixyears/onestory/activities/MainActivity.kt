be17e01a22562287025c45a825eb96058dbca6e6
Tharunkumarpallapu21 <tharunpallapu2006@gmail.com>
2026-06-12 16:36:07 +0530

    Added birthday auto redirect

--- app/src/main/java/com/sixyears/onestory/activities/MainActivity.kt
+++ app/src/main/java/com/sixyears/onestory/activities/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.sixyears.onestory.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.sixyears.onestory.R
 import com.sixyears.onestory.countdown.CountdownManager
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,24 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        val calendar = Calendar.getInstance()
+
+        val month = calendar.get(Calendar.MONTH) + 1
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        if (month == 6 && day == 25) {
+
+            startActivity(
+                Intent(
+                    this,
+                    BirthdayActivity::class.java
+                )
+            )
+
+            finish()
+            return
+        }
+
         val countdown =
             findViewById<TextView>(
                 R.id.txtCountdown
@@ -20,8 +40,8 @@ class MainActivity : AppCompatActivity() {
 
         val days =
             CountdownManager.getDaysUntilBirthday(
-                8,
-                15
+                6,
+                25
             )
 
         countdown.text =
