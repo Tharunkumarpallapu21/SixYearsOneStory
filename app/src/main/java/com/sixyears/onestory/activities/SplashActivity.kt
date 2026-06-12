b98b7f03b46fd9880e7a3ac7afac3ceeb418412e
Tharunkumarpallapu21 <tharunpallapu2006@gmail.com>
2026-06-12 16:29:57 +0530

    Added splash welcome and main screens

---
+++ app/src/main/java/com/sixyears/onestory/activities/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.sixyears.onestory.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.sixyears.onestory.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            startActivity(
+                Intent(
+                    this,
+                    WelcomeActivity::class.java
+                )
+            )
+
+            finish()
+
+        }, 3000)
+
+    }
+}
---
+++ app/src/main/java/com/sixyears/onestory/activities/WelcomeActivity.kt
@@ -0,0 +1,31 @@
+package com.sixyears.onestory.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.sixyears.onestory.R
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_welcome)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java
+                )
+            )
+
+            finish()
+
+        }, 5000)
+
+    }
+}
