ddfbf4ff609d1e65e4acd58b121dd6cd9bcd5137
Tharunkumarpallapu21 <tharunpallapu2006@gmail.com>
2026-06-12 16:54:20 +0530

    Added final surprise screen

---
+++ app/src/main/java/com/sixyears/onestory/activities/FinalSurpriseActivity.kt
@@ -0,0 +1,14 @@
+package com.sixyears.onestory.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sixyears.onestory.R
+
+class FinalSurpriseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_final_surprise)
+    }
+}
